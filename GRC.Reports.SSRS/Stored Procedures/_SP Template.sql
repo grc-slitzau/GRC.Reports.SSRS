3e89e5c872b795bee9afb6496749e1002b37e97e
Scott Litzau <slitzau@grandrapidschair.com>
2021-05-03 08:38:23 -0500

    Add project files.

---
+++ GRC.Reports.SSRS/Stored Procedures/_SP Template.sql
@@ -0,0 +1,23 @@
+﻿IF EXISTS (SELECT * FROM sysobjects WHERE type = 'P' AND name = 'StoredProcName')
+	BEGIN
+		DROP  Procedure  StoredProcName
+	END
+
+GO
+
+CREATE Procedure StoredProcName
+(
+	@Param INT
+)
+WITH EXEC AS owner
+AS
+
+SET NOCOUNT ON;
+
+
+
+SET NOCOUNT OFF;
+
+GO
+
+GRANT EXEC ON StoredProcName TO [GRCSERVER1\GRC All]
